c75e11ed9929478f1f6b5164bdc9c5298dd34455
daro-asecas <98608003+daro-asecas@users.noreply.github.com>
2022-06-30 00:43:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,15 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-// const mongoose = require('mongoose')
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-// const db = mongoose.connection
-// db.on('error', error => console.error(error))
-// db.once('open', () => console.log('Connected to Mongoose'))
+// maniobra para deployar en heroku pero sin conectar la local
+
+if (process.env.DATABASE_URL=="mongodb://localhost/mybrary") {
+    const mongoose = require('mongoose')
+    mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+    const db = mongoose.connection
+    db.on('error', error => console.error(error))
+    db.once('open', () => console.log('Connected to Mongoose'))
+}
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
