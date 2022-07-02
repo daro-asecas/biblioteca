3acb1ec73033f6abfaa77b02500066df1e2f4087
Daro Asecas <daro.1919@gmail.com>
2022-07-01 21:51:40 -0300

    terminando V3, no anda la carga de archivos

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -18,7 +19,6 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 // maniobra para deployar en heroku pero sin conectar la local
-
 if (process.env.DATABASE_URL!=="mongodb://localhost/mybrary") {
     const mongoose = require('mongoose')
     mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -29,5 +29,6 @@ if (process.env.DATABASE_URL!=="mongodb://localhost/mybrary") {
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
